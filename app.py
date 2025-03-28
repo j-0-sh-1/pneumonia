cd00801f170fba7c7ee429d567570df9ff4d59f7
j-0-sh-1 <78529754+j-0-sh-1@users.noreply.github.com>
2025-03-28 21:14:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def process_image(uploaded_file):
 st.title("Pneumonia X-ray Analysis")
 st.write("Upload your X-ray image. The app will classify the image and highlight regions contributing to the model's decision.")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="unique_file_uploader")
 
 if uploaded_file is not None:
     x, orig_img = process_image(uploaded_file)
